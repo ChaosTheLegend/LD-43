5532d9df9fc893609cb2438792db0d1345e8c614
Titanium900 <19palacewinston@bprep.org>
2018-12-01 16:05:49 -0800

    Added Items
    - Sword(WIP)
    -Chest(WIP)
    -Fixed Bug (Forgot what it was)

---
+++ LD43 project/Assets/Scripts/CameraMovment.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovment : MonoBehaviour {
+    public GameObject player;
+    public float x;
+    public float y;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        x = player.transform.position.x;
+        y = player.transform.position.y;
+        transform.position = new Vector3(x, y, -10);
+        //transform.position = Vector2.MoveTowards(transform.position, player.transform.position, 1 * Time.deltaTime);
+    }
+}
--- LD43 project/Assets/Scripts/CharacterMovement.cs
+++ LD43 project/Assets/Scripts/CharacterMovement.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour {
     public float speed;
-
+    int turn = 1;
+    public SpriteRenderer rend;
     Rigidbody2D rb;
 
     float x;
@@ -18,6 +19,19 @@ public class CharacterMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if (Input.GetKeyDown("d"))
+        {
+            turn = 1;
+            //transform.localScale = new Vector2(-1, 1);
+            //rend.flipX = false;
+        }
+        if (Input.GetKeyDown("a"))
+        {
+            turn = -1;
+            //transform.localScale = new Vector2(1, -1);
+            //rend.flipX = true;
+        }
+        transform.localScale = new Vector2(1*turn, 1);
         x = Input.GetAxisRaw("Horizontal");
         y = Input.GetAxisRaw("Vertical");
         rb.velocity = new Vector2(x,y)*speed;
