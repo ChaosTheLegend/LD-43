9830bcde8cfcd9d8ef0fa7fecfb851be8bcf874b
ChaosTheLegend <chaosthelegend@inbox.ru>
2018-12-03 10:41:34 +0600

    Working HUD

---
+++ LD43 project/Assets/HealthControll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthControll : MonoBehaviour {
+    public int Health;
+    public float Cooldown;
+    float tm;
+    private void Update()
+    {
+        if (tm > 0)
+        {
+            tm -= Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            tm = Cooldown;
+            Health--;
+        }
+    }
+
+
+}
--- LD43 project/Assets/Scripts/HUD.cs
+++ LD43 project/Assets/Scripts/HUD.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class HUD : MonoBehaviour {
-    public Canvas ui;
-    public GameObject health;
-    public GameObject inventoryBackground;
-	// Use this for initialization
-	void Start () {
-        
+    public Image[] Slots;
+    public Image Health;
+    public Sprite[] HPStates;
+
+
+    // Update is called once per frame
+    void Update () {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        GameObject RotatingThing = player.GetComponent<InventoryC>().RotatingThing;
+        int hp = player.GetComponent<HealthControll>().Health;
+
+        try
+        {
+            if (hp > 0)
+            {
+                Health.sprite = HPStates[hp - 1];
+            }
+            Slots[0].sprite = RotatingThing.GetComponent<ItemControll>().weapon[2].sprite;
+            Slots[1].sprite = RotatingThing.GetComponent<ItemControll>().weapon[0].sprite;
+            Slots[2].sprite = RotatingThing.GetComponent<ItemControll>().weapon[1].sprite;
+        }
+        catch { }
+
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
