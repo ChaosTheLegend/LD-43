40efc6336d42275d7cbf36e3ea34892cb5047dc3
Titanium900 <19palacewinston@bprep.org>
2018-12-02 17:00:27 -0800

    HUD added

---
+++ LD43 project/Assets/Scripts/HUD.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUD : MonoBehaviour {
+    public Canvas ui;
+    public GameObject health;
+    public GameObject inventoryBackground;
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
