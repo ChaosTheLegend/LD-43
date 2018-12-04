7f34fe1066d686bf2e18e19fc44fb359b8bd070a
Titanium900 <Titanium90000@Gmail.com>
2018-12-03 16:24:53 -0800

    UI Mostly Done

---
+++ LD43 project/Assets/UI/ButtonButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonButton : MonoBehaviour
+{
+    public Button gButton;
+    void Start()
+    {
+        Button btn2 = gButton.GetComponent<Button>();
+        btn2.onClick.AddListener(TaskOnClick);
+    }
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene("TestingScene");
+    }
+}
