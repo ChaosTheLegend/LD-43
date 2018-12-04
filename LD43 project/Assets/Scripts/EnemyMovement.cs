6e4f93755349c7b041cda7aa58442c406221a2d4
Titanium900 <Titanium90000@Gmail.com>
2018-12-03 17:29:03 -0800

    EnemyUpdate

--- LD43 project/Assets/Scripts/EnemyMovement.cs
+++ LD43 project/Assets/Scripts/EnemyMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour {
     Rigidbody2D rb;
     public int speed;
+    bool move;
     public bool active = false;
     float x;
     float y;
@@ -12,23 +13,32 @@ public class EnemyMovement : MonoBehaviour {
     void Start () {
         rb = GetComponent<Rigidbody2D>();
         new WaitForSeconds(8);
+        move = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
         GameObject player = GameObject.FindGameObjectWithTag("Player");
-            if (player.transform.position.x > transform.position.x)
+            if (player.transform.position.x > transform.position.x && move == true)
                 x = 1;
-            if (player.transform.position.x == transform.position.x)
+            if (player.transform.position.x == transform.position.x && move == true)
                 x = 0;
-            if (player.transform.position.x < transform.position.x)
+            if (player.transform.position.x < transform.position.x && move == true)
                 x = -1;
-            if (player.transform.position.y > transform.position.y)
+            if (player.transform.position.y > transform.position.y && move == true)
                 y = 1;
-            if (player.transform.position.y == transform.position.y)
+            if (player.transform.position.y == transform.position.y && move == true)
                 y = 0;
-            if (player.transform.position.y < transform.position.y)
+            if (player.transform.position.y < transform.position.y && move == true)
                 y = -1;
             rb.velocity = new Vector2(speed * x, speed * y);
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            move = true;
+        }
+            
     }
 }
