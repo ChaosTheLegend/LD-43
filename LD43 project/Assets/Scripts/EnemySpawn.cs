20d0479ec8960a9621d40535af050598caa92498
Titanium900 <Titanium90000@Gmail.com>
2018-12-03 17:01:02 -0800

    I hope this doesn't break

---
+++ LD43 project/Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour {
+    public GameObject enemy;
+	// Use this for initialization
+	void Start () {
+        Instantiate(enemy, transform.position, new Quaternion(0,0,0,0));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
