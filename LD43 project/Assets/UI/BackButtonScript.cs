b0041e52a844319f5b5a0ea820d657c1a554a380
Titanium900 <Titanium90000@Gmail.com>
2018-12-03 16:40:46 -0800

    Menu Done
    UI

---
+++ LD43 project/Assets/UI/BackButtonScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackButtonScript : MonoBehaviour
+{
+    public Button bButton;
+    void Start()
+    {
+        Button btn4 = bButton.GetComponent<Button>();
+        btn4.onClick.AddListener(TaskOnClick);
+    }
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ LD43 project/Assets/UI/CreditsButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsButton : MonoBehaviour
+{
+    public Button cButton;
+    void Start()
+    {
+        Button btn3 = cButton.GetComponent<Button>();
+        btn3.onClick.AddListener(TaskOnClick);
+    }
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+}
